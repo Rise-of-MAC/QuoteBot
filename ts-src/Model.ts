7d801535ecbb1f7e10390bfb7e606317e2636ea5
Gildas Houlmann <gildas.houlmann@gmail.com>
2021-01-04 22:42:50 +0100

    update Model.ts

--- ts-src/Model.ts
+++ ts-src/Model.ts
@@ -1,50 +1,23 @@
-export type Movie = {
+export type Quote = {
   _id: string;
-  rank: number;
-  title: string;
-  genre: string;
-  description: string;
-  director: string;
-  actors: string;
-  year: number;
-  runtime: number;
-  rating: number;
-  votes: number;
-  revenue: number;
-  metascore: number;
-}
-
-
-export type Actor = {
-  id: number;
-  name: string;
-}
-
-export type Genre = {
-  id: number;
-  name: string;
-}
-
-export type Comment = {
-  id: number;
   text: string;
-  at: Date;
+  author: Author;
+  tags: string;
+  likes: number;
+  language: string;
+  added: Date;
 }
 
-export type Added = {
-  at: Date;
+
+export type Author = {
+  id: number;
+  name: string;
 }
 
-export type Requested = {
-  at: Date;
+export type Tag = {
+  id: number;
+  name: string;
 }
-
-export type Liked = {
-  at: Date;
-  rank: number;
-}
-
-export const likedValues = [1,2,3,4,5];
 
 export type User = {
   username?: string;
